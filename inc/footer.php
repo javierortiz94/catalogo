87d44b88882c4405448329a6b3ac658a74f1c133
javierortiz94 <33522355+javierortiz94@users.noreply.github.com>
2017-11-09 09:53:18 -0600

    Catalogo
    Proyecto de estadías.

---
+++ inc/footer.php
@@ -0,0 +1,25 @@
+<footer>
+    <h3 class="text-center">Siguenos en</h3><br>
+    <ul class="list-unstyled text-center">
+        <a href="https://www.facebook.com/vianney.empresa/" target="_blank" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Facebook" >
+            <img src="assets/icons/social-facebook.png" alt="facebook-icon" >
+        </a>
+        <a href="https://www.instagram.com/vianney_oficial/" target="_blank" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Instagram" >
+            <img src="assets/icons/social-instagram.png" alt="facebook-icon" >
+        </a>
+        <a href="https://www.youtube.com/user/vianneyoficial" target="blank" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Google +">
+            <img src="assets/icons/social-youtube.png" alt="googleplus-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Linkedin">
+            <img src="assets/icons/social-linkedin.png" alt="linkedin-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Pinterest">
+            <img src="assets/icons/social-pinterest.png" alt="pinterest-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Twitter">
+            <img src="assets/icons/social-twitter.png" alt="twitter-icon">
+        </a>
+    </ul>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo">Vianney Textil Hogar &copy; 2017</h5>
+</footer>
